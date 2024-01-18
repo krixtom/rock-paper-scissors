cde4027d37c1fcfc8020a0369713f37b0068fe63
Kristijan <tomickristijansb@live.com>
2024-01-18 12:08:36 +0100

    add getPlayerChoice function and playRound function in index.js add Play game button in index.html

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
+const playBtn = document.getElementById('play-btn')
+
+
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors']
     const randomNumber = Math.floor(Math.random() * 3)
     const choice = choices[randomNumber]
     return choice
-
+    
 }
-console.log('hi')
+
+function getPlayerChoice(){
+    const playerChoice = prompt("Choose Rock, Paper or Scissors").toLowerCase()
+    return playerChoice
+}
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        alert('It is a tie!')
+    } else if(playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock' ){
+        alert('Player wins')
+    } else {
+        alert('Computer wins')
+    }
+    
+}
+
+
+
+playBtn.addEventListener('click', () => {
+    playRound(getPlayerChoice(),getComputerChoice())
+})
