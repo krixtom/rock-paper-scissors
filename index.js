9ccf9fd9d9ddaeb36608d0107c5e06dac9e88050
Kristijan <tomickristijansb@live.com>
2024-02-02 20:11:33 +0100

    add ne ui

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-const playBtn = document.getElementById('play-btn')
+const selectionBtns = document.querySelectorAll('button')
+const player = document.querySelector('.player')
+const computer = document.querySelector('.computer')
+const message = document.querySelector('.result-container')
+
+let playerScore = 0
+let computerScore = 0
+
+
+function updateScore(winner) {
+    winner++
+    return winner
+
+}
+
+
+selectionBtns.forEach((btn) =>  {
+    let playerSelection = ''
+    btn.addEventListener('click', (e) => {
+        playerSelection = e.target.textContent.toLowerCase()
+        playRound(playerSelection, getComputerChoice())
+    })
+
+
+
+
+})
 
 
 
@@ -10,24 +36,32 @@ function getComputerChoice() {
     
 }
 
-function getPlayerChoice(){
-    const playerChoice = prompt("Choose Rock, Paper or Scissors").toLowerCase()
-    return playerChoice
-}
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
-        alert('It is a tie!')
+        console.log('It is a tie!')
     } else if(playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock' ){
-        alert('Player wins')
+        playerScore = updateScore(playerScore)
+        player.textContent = `Player: ${playerScore}`
+        if(playerScore === 5) {
+            message.innerHTML = `<p>Player Wins!</p>`
+        
+        }
+        
     } else {
-        alert('Computer wins')
+        computerScore = updateScore(computerScore)
+        computer.textContent = `Computer: ${computerScore}`
+        if(computerScore === 5) {
+            message.innerHTML = `<p>Computer Wins!</p>`
+        
+        }
+
+
     }
     
 }
 
 
 
-playBtn.addEventListener('click', () => {
-    playRound(getPlayerChoice(),getComputerChoice())
-})
+
+
